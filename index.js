6565cd344787334beba7a393793502c6e7d4c513
Paulius B <paulspremium@gmail.com>
2021-09-05 23:22:10 +0300

    exersize 1 to 6 done

--- index.js
+++ index.js
@@ -7,14 +7,116 @@ app.init = async () => {
     const connection = await mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        database: 'your_database_name',
+        database: 'mushroom',
     });
 
     let sql = '';
     let rows = [];
 
     // LOGIC BELOW
+    sql = 'SELECT * FROM `mushroom`';
+    [rows] = await connection.execute(sql);
+    // console.log(rows);
 
+    // ** 1. ** _Isspausdinti, visu grybu pavadinimus ir ju kainas, grybus isrikiuojant nuo brangiausio link pigiausio_
+
+    sql = 'SELECT `mushroom`, `price`\
+            FROM`mushroom` \
+            ORDER BY `price` DESC;';
+    [rows] = await connection.execute(sql);
+
+    // const mushroomList = rows.map(obj => obj.mushroom);
+    // const mushroomPriceList = rows.map(obj => obj.price);
+
+    let count = 0;
+    let allMushrooms = [];
+    for (const { mushroom, price } of rows) {
+        allMushrooms.push(`${++count}. ${mushroom[0].toUpperCase()}${mushroom.slice(1)} - ${price} EUR/kg`)
+    }
+    console.log(allMushrooms.join('\n'));
+
+    //** 2. ** _Isspausdinti, visu grybautoju vardus_
+
+    sql = 'SELECT `name` \
+            FROM `gatherer`';
+
+    [rows] = await connection.execute(sql);
+    const gatherers = rows.map(obj => obj.name);
+
+    console.log('**************************************');
+    console.log(`Grybautojai: ${gatherers.join(', ')}.`);
+
+    // ** 3. ** _Isspausdinti, brangiausio grybo pavadinima_
+
+    sql = 'SELECT `mushroom` as name, `price`\
+            FROM`mushroom` \
+            ORDER BY `price` DESC;';
+
+    [rows] = await connection.execute(sql);
+
+    console.log('********************************');
+    console.log(`Brangiausias grybas yra: ${rows[0].name}.`);
+
+    //**4.** _Isspausdinti, pigiausio grybo pavadinima_
+    const last = rows.length - 1;
+    console.log('********************************');
+    console.log(`Pigiausais grybas yra: ${rows[last].name}.`);
+
+
+    //**5.** _Isspausdinti, visu kiek vidutiniskai reikia grybu, jog jie svertu 1 kilograma (suapvalinti iki vieno skaiciaus po kablelio), isrikiuojant pagal pavadinima nuo abeceles pradzios link pabaigos_ *******dalinsim is 1000g kad gauti kieki
+    sql = 'SELECT `mushroom`, (1000/`weight`) as kiekis\
+            FROM`mushroom` \
+            ORDER BY `mushroom` ASC;';
+
+    [rows] = await connection.execute(sql);
+
+    count = 0;
+    allMushrooms = [];
+    for (const { mushroom, kiekis } of rows) {
+        allMushrooms.push(`${++count}) ${mushroom} - ${(+kiekis).toFixed(1)}`)
+    }
+    console.log('*****************************');
+    console.log('Grybai:');
+    console.log(allMushrooms.join('\n'));
+
+    //**6.** _Isspausdinti, visu grybautoju krepselyje esanciu grybu kiekius (issirikiuojant pagal grybautojo varda nuo abeceles pradzios link pabaigos)_
+    sql = 'SELECT `gatherer`.`name`, `gatherer`.`id`\
+            FROM`gatherer` \
+            ORDER BY `id` ASC;';
+
+    const sql2 = 'SELECT `basket`.`gatherer_id` as grybautojas, `count`\
+            FROM`basket`\
+            ORDER BY `gatherer_id` ASC;';
+
+    [rows] = await connection.execute(sql);
+
+    let grybautojai = [];
+
+    for (const { name } of rows) {
+        grybautojai.push(name)
+    }
+
+    [rows] = await connection.execute(sql2);
+    let infoList = []
+
+    for (i = 0; i < grybautojai.length; i++) {
+        const person = grybautojai[i];
+        let grybaiCount = 0;
+
+        for (const { grybautojas, count } of rows) {
+            if (grybautojas === i + 1) {
+                grybaiCount = grybaiCount + count;
+            }
+        }
+        infoList.push({ person, grybaiCount })
+    }
+
+    let rowCount = 0;
+    console.log(`**************************************`);
+    console.log(`Grybu kiekis pas grybautoja:`);
+    for (const { person, grybaiCount } of infoList) {
+        console.log(`${++rowCount}) ${person} - ${grybaiCount} grybu`);
+    }
 
 }
 
